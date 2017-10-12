87fb430e23b7fc3548921aa494fbc0899b9d2af7
HLLedford <ledfordhl@email.wofford.edu>
2017-10-12 18:12:32 -0400

    First ArgumentParser commit. Beginning of Feature1.

--- src/main/java/edu/wofford/ArgumentParser.java
+++ src/main/java/edu/wofford/ArgumentParser.java
@@ -1,6 +1,31 @@
 package edu.wofford;
 
+import java.util.*;
+
 public class ArgumentParser {
-
-
+    private List<String> argumentNames;
+    
+    private List<String> argumentValues;
+    
+    public ArgumentParser(String[] names, String[] values) {
+        argumentNames = new ArrayList<String>();
+        
+        argumentValues = new ArrayList<String>();
+        
+        for (String name: names) {
+            argumentNames.add(name);
+        }
+        
+        for (String value: values) {
+            argumentValues.add(value);
+        }
+    }
+    
+    public String getValue(String valueName) {
+        return argumentValues.get(argumentNames.indexOf(valueName));
+    }
+    
+    public String getValue(int valuePosition) {
+        return argumentValues.get(valuePosition);
+    }
 }
