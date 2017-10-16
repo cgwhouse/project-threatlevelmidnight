2b0b4175c774aac9249325bf985a76d44d984cd1
HLLedford <ledfordhl@email.wofford.edu>
2017-10-16 13:48:32 -0400

    Adding unit test

--- src/test/java/edu/wofford/ArgumentParserTest.java
+++ src/test/java/edu/wofford/ArgumentParserTest.java
@@ -7,18 +7,23 @@ import static org.junit.Assert.*;
 public class ArgumentParserTest {
 	private ArgumentParser parser;
 
-	private	String[] argumentNames = {"length", "width", "height"};
+	// private	String[] argumentNames = {"length", "width", "height"};
 
-	private	String[] argumentValues = {"7", "5", "2"};
+	// private	String[] argumentValues = {"7", "5", "2"};
     
-    @Before
-    public void setUp() {
-		parser = new ArgumentParser(argumentNames, argumentValues);
-    }
+    // @Before
+    // public void setUp() {
+		// parser = new ArgumentParser(argumentNames, argumentValues);
+    // }
     
-
 	@Test
 	public void testInitialParserIsCorrect() {
+        String[] argumentNames = {"length", "width", "height"};
+
+        String[] argumentValues = {"7", "5", "2"};
+        
+        parser = new ArgumentParser(argumentNames, argumentValues);
+    
 		for (int i = 0; i < 3; i++) {
 			assertEquals(argumentValues[i], parser.getValue(i));
 		}
@@ -28,4 +33,12 @@ public class ArgumentParserTest {
 		}
 	}
     
+    @Test()
+    public void testInitialParserIsIncorrect() {
+        String[] argumentNames = {"length", "width", "height"};
+
+        String[] argumentValues = {"7", "5", "2", "3"};
+        
+        parser = new ArgumentParser(argumentNames, argumentValues);
+    }
 }
