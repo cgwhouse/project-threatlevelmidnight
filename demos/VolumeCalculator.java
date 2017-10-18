a335adee1303ac9caf45433fbd27855a19fa63a8
DK <kaplunda@email.wofford.edu>
2017-10-18 13:51:37 -0400

    Refactor demo volume calculator

--- demos/VolumeCalculator.java
+++ demos/VolumeCalculator.java
@@ -5,7 +5,9 @@ public class VolumeCalculator {
     public static void main(String[] args) {
 		String[] measurementNames = {"length", "width", "height"};
 
-        ArgumentParser parser = new ArgumentParser(measurementNames, args);
+        ArgumentParser parser = new ArgumentParser();
+        parser.setProgramNames(measurementNames);
+        parser.setProgramValues(args);
 
         double d = Double.parseDouble(parser.getValue("length")) * Double.parseDouble(parser.getValue("width")) * Double.parseDouble(parser.getValue("height"));
 
