70e70af83c5b686ec14f81601fdf00318bec236f
DK <kaplunda@email.wofford.edu>
2017-10-18 12:53:20 -0400

    Make working demo of Volume calculator

--- demos/VolumeCalculator.java
+++ demos/VolumeCalculator.java
@@ -1,12 +1,15 @@
-package edu.wofford;
+import edu.wofford.*;
 
 public class VolumeCalculator {
 
     public static void main(String[] args) {
+		String[] measurementNames = {"length", "width", "height"};
 
-        Calculator volume = new Calculator(args);
+        ArgumentParser parser = new ArgumentParser(measurementNames, args);
 
-        System.out.println(volume.calculate());
+        double d = Double.parseDouble(parser.getValue("length")) * Double.parseDouble(parser.getValue("width")) * Double.parseDouble(parser.getValue("height"));
+
+        System.out.println(d);
 	}
 
 }
