f9d81ba3be57241cdf9ac62272f6454f734ea6f4
Cristian Widenhouse <cgwidenhouse@gmail.com>
2017-10-19 09:27:40 -0400

    Implement keywords for Feature 01 acceptance test

--- src/main/java/keywords/Feature01.java
+++ src/main/java/keywords/Feature01.java
@@ -3,44 +3,59 @@ package keywords;
 import edu.wofford.*;
 
 public class Feature01 {
+	ArgumentParser parser;
+	ArgumentParser absurdParser;
 
 	public void startProgramWithArguments(String[] args) {
-		
+		ArgumentParser parser = new ArgumentParser();
+		String[] names = { "length", "width", "height" };
+		// parser.setProgramName("VolumeCalculator");
+		parser.setProgramNames(names);
+		parser.setProgramValues(args);
 	}
-	
+
 	public String getLength() {
-		return "7";
+		return parser.getValue("length");
 	}
-	
+
 	public String getWidth() {
-		return "5";
+		return parser.getValue("width");
 	}
 
 	public String getHeight() {
-		return "2";
+		return parser.getValue("height");
 	}
-	
+
 	public String getProgramOutput() {
-		return "70";
+		double d = Double.parseDouble(parser.getValue("length")) * Double.parseDouble(parser.getValue("width"))
+				* Double.parseDouble(parser.getValue("height"));
+		return Double.toString(d);
 	}
-	
+
 	public void startAbsurdProgramWithArguments(String[] args) {
-		
+		absurdParser = new ArgumentParser();
+		// String[] names = { "length", "width", "height" };
+		// parser.setProgramName("VolumeCalculator");
+		// parser.setProgramNames(names);
+		// parser.setProgramValues(args);
+		String[] names = { "pet", "number", "rainy", "bathrooms" };
+		absurdParser.setProgramNames(names);
+		absurdParser.setProgramValues(args);
 	}
-	
+
 	public String getPet() {
-		return "dog";
+		return absurdParser.getValue("pet");
 	}
-	
+
 	public String getNumber() {
-		return "2";
+		return absurdParser.getValue("number");
 	}
-	
+
 	public String getRainy() {
-		return "true";
+		return absurdParser.getValue("rainy");
 	}
-	
+
 	public String getBathrooms() {
-		return "3.5";
+		return absurdParser.getValue("bathrooms");
 	}
 }
