2ecaca288bcab9839b73ad37065450ebc65a247d
Cristian Widenhouse <cgwidenhouse@gmail.com>
2017-10-20 13:11:29 -0400

    Formatting

--- src/main/java/keywords/Feature02.java
+++ src/main/java/keywords/Feature02.java
@@ -3,26 +3,27 @@ package keywords;
 import edu.wofford.*;
 
 public class Feature02 {
-	ArgumentParser parser;
-	ArgumentParser absurdParser;
+        ArgumentParser parser;
+        ArgumentParser absurdParser;
 
-	public void startProgramWithArguments(String[] args) {
-        String[] argumentNames = {"length", "width", "height"};
-        String[] argumentDescriptions = {"length the length of the box (float)", "width the width of the box (float)", "height the height of the box (float)"};
-        parser = new ArgumentParser();
-        parser.setProgramName("VolumeCalculator");
-        parser.setProgramDescription("Calculate the volume of a box.");
-        parser.setArgumentDescriptions(argumentDescriptions);
-        parser.setProgramNames(argumentNames);
-	}
+        public void startProgramWithArguments(String[] args) {
+                String[] argumentNames = { "length", "width", "height" };
+                String[] argumentDescriptions = { "length the length of the box (float)",
+                                "width the width of the box (float)", "height the height of the box (float)" };
+                parser = new ArgumentParser();
+                parser.setProgramName("VolumeCalculator");
+                parser.setProgramDescription("Calculate the volume of a box.");
+                parser.setArgumentDescriptions(argumentDescriptions);
+                parser.setProgramNames(argumentNames);
+        }
 
-	public String getProgramOutput() {
-        try {
-            String[] argumentValues = {"-h"};
-            parser.setProgramValues(argumentValues);
-            return "Not passing robot test 2.";
-		} catch (ArgumentException e) {
-			return e.getMessage();
-		}
-	}
+        public String getProgramOutput() {
+                try {
+                        String[] argumentValues = { "-h" };
+                        parser.setProgramValues(argumentValues);
+                        return "Not passing robot test 2.";
+                } catch (ArgumentException e) {
+                        return e.getMessage();
+                }
+        }
 }
