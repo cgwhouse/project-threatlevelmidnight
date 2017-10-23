bd5abcd6b1fad2d64e836c3e8337d96c0add02e3
Cristian Widenhouse <cgwidenhouse@gmail.com>
2017-10-23 13:39:50 -0400

    Formatting

--- demos/VolumeCalculator.java
+++ demos/VolumeCalculator.java
@@ -3,15 +3,16 @@ import edu.wofford.*;
 public class VolumeCalculator {
 
     public static void main(String[] args) {
-		String[] measurementNames = {"length", "width", "height"};
+        String[] measurementNames = { "length", "width", "height" };
 
         ArgumentParser parser = new ArgumentParser();
         parser.setProgramNames(measurementNames);
         parser.setProgramValues(args);
 
-        double d = Double.parseDouble(parser.getValue("length")) * Double.parseDouble(parser.getValue("width")) * Double.parseDouble(parser.getValue("height"));
+        double d = Double.parseDouble(parser.getValue("length")) * Double.parseDouble(parser.getValue("width"))
+                * Double.parseDouble(parser.getValue("height"));
 
         System.out.println(d);
-	}
+    }
 
 }
