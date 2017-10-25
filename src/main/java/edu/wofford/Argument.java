9c2b206069a5dd1c1fb52510961f01539524f3fd
Cristian Widenhouse <cgwidenhouse@gmail.com>
2017-10-25 14:20:55 -0400

    Add Argument class as part of refactoring

---
+++ src/main/java/edu/wofford/Argument.java
@@ -0,0 +1,43 @@
+package edu.wofford;
+
+import java.util.*;
+
+public class Argument {
+
+    private String name;
+    private String value;
+    private String description;
+    private String type;
+
+    public Argument(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
