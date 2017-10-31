0f6fe23415d5ee09d0acb235cc1d7422cc84bf2f
Cristian Widenhouse <cgwidenhouse@gmail.com>
2017-10-30 21:29:23 -0400

    Implement keywords to run acceptance tests for features 4 and 5

---
+++ src/main/java/keywords/Feature05.java
@@ -0,0 +1,45 @@
+package keywords;
+
+import edu.wofford.*;
+
+public class Feature05 {
+    ArgumentParser parser;
+
+    public void startProgramWithArguments(String[] args) {
+        parser = new ArgumentParser();
+        parser.setProgramName("VolumeCalculator");
+        Argument typeArg = new Argument("--type");
+        Argument digitsArg = new Argument("--digits");
+        typeArg.setValue("box");
+        digitsArg.setValue("4");
+        parser.setArgument(typeArg);
+        parser.setArgument(digitsArg);
+
+        String[] names = { "length", "width", "height" };
+        for (int i = 0; i < names.length; i++) {
+            parser.setArgument(names[i]);
+        }
+        parser.setArgumentValues(args);
+    }
+
+    public String getLength() {
+        return parser.getValue("length");
+    }
+
+    public String getWidth() {
+        return parser.getValue("width");
+    }
+
+    public String getHeight() {
+        return parser.getValue("height");
+    }
+
+    public String getType() {
+        return parser.getValue("--type");
+    }
+
+    public String getDigits() {
+        return parser.getValue("--digits");
+    }
+
+}
