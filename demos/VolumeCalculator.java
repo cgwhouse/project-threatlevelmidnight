6b9de689f9221facc4d4c1ce635cb867a2d1986e
HLLedford <ledfordhl@email.wofford.edu>
2017-11-02 17:14:32 -0400

    Demos are done for features 01 to 05

--- demos/VolumeCalculator.java
+++ demos/VolumeCalculator.java
@@ -3,24 +3,48 @@ import edu.wofford.*;
 public class VolumeCalculator {
 
     public static void main(String[] args) {
-        String[] measurementNames = { "length", "width", "height" };
-        String[] argDescriptions = { "the length of the box", 
-									"the width of the box", 
-									"the height of the box"};
+        System.out.println("Example 1: VolumeCalculator 5 --type square 4 3\n");
 
         ArgumentParser parser = new ArgumentParser();
-        
-        parser.setProgramName("Volume Calculator");
-        parser.setProgramDescription("Calculates the volume of a box");
-        parser.setArgumentDescriptions(argDescriptions);
-        
-        parser.setProgramNames(measurementNames);
-        parser.setProgramValues(args);
+        String[] requiredArguments = { "width", "height" };
 
-        double d = Double.parseDouble(parser.getValue("length")) * Double.parseDouble(parser.getValue("width"))
-                * Double.parseDouble(parser.getValue("height"));
+        Argument lengthArg = new Argument("length");
+        lengthArg.setType("float");
+        lengthArg.setDescription("the length of the box");
 
-        System.out.println(d);
+        String programName = "Volume Calculator";
+        String programDescription = "Calculates the volume of a box";
+        String[] argumentsValues = { "5", "--type", "square", "4", "3" };
+        String[] helpValue = { "-h" };
+        String[] requiredArgumentsDescriptions = { "the width of the box", "the height of the box" };
+        String[] requiredArgumentsTypes = { "float", "float"};
+        
+        Argument arg = new Argument("--type");
+        arg.setValue("ellipsoid");
+        
+        parser.setProgramName(programName);
+        parser.setProgramDescription(programDescription);
+        parser.setArgument(lengthArg);
+        parser.setArguments(requiredArguments);
+        parser.setArgument(arg);
+        for (int i = 0; i < 2; i++) {
+            parser.setArgumentType(requiredArguments[i], requiredArgumentsTypes[i]);
+            parser.setArgumentDescription(requiredArguments[i], requiredArgumentsDescriptions[i]);
+        }
+
+        parser.setArgumentValues(argumentsValues);
+
+        System.out.println("The value of width should be 4... it is... " + parser.getValue("width"));
+        System.out.println("The type of length should be float... it is... " + lengthArg.getType());
+        System.out.println("The value of --type should be square... it is... " + parser.getValue("--type"));
+
+        System.out.println("\nExample 2: VolumeCalculator -h\n");
+        System.out.println("The following help message is displayed when passing -h...");
+
+        try {
+            parser.setArgumentValues(helpValue);
+        } catch (final ArgumentException error) {
+            System.out.println(error.getMessage());
+        }
     }
-
 }
