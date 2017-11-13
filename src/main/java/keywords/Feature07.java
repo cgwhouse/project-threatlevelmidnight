64423c6c29f6c77beb4ceed37d2a802923bba166
DK <kaplunda@email.wofford.edu>
2017-11-13 13:45:07 -0500

    Add Feature 7 acceptance test

---
+++ src/main/java/keywords/Feature07.java
@@ -0,0 +1,45 @@
+package keywords;
+
+import edu.wofford.*;
+
+public class Feature07 {
+    ArgumentParser parser;
+
+    public void startProgramWithArguments(String[] args) {
+        parser = new ArgumentParser("VolumeCalculator");
+        Argument typeArg = new Argument("--type");
+        Argument digitsArg = new Argument("--digits");
+        typeArg.setValue("box");
+        digitsArg.setValue("4");
+        
+        parser.setNamedArgument(typeArg, "-t");
+        parser.setNamedArgument(digitsArg, "-d");
+
+        String[] names = { "length", "width", "height" };
+        for (int i = 0; i < names.length; i++) {
+            parser.setArgument(names[i]);
+        }
+        parser.setArgumentValues(args);
+    }
+
+    public String getLength() {
+        return parser.getValue("length");
+    }
+
+    public String getWidth() {
+        return parser.getValue("width");
+    }
+
+    public String getHeight() {
+        return parser.getValue("height");
+    }
+
+    public String getType() {
+        return parser.getValue("--type");
+    }
+
+    public String getDigits() {
+        return parser.getValue("--digits");
+    }
+
+}
