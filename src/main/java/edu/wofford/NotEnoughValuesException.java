d79ea0aae196e5718de1ef6604f91bc276cc7ddc
Cristian Widenhouse <cgwidenhouse@gmail.com>
2017-12-01 15:04:13 -0500

    Add new NotEnoughValuesException

---
+++ src/main/java/edu/wofford/NotEnoughValuesException.java
@@ -0,0 +1,17 @@
+package edu.wofford;
+
+/** 
+* Thrown to indicate that ArgumentParser has parsed a positional argument that requires more values than those
+* provided. 
+*/
+public class NotEnoughValuesException extends ArgumentException {
+
+    /** 
+    * Constructs a NotEnoughValuesException with the specified detail message.
+    *
+    * @param message the detail message
+    */
+    public NotEnoughValuesException(String msg) {
+        super(msg);
+    }
+}
